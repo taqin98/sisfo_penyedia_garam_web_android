73597514fa7fb4322c82a4fec120b0c26946aa9f
Nurul Muttaqin <taqin98@Nuruls-MacBook-Pro.local>
2021-01-05 14:44:15 +0700

    update register

--- application/views/register.php
+++ application/views/register.php
@@ -107,7 +107,7 @@
 
             <!-- Buyer Register -->
             <div class="tab-pane fade active show" id="pilled" role="tabpanel">
-buyer
+<!-- buyer -->
                 <div class="section full mt-1">
                     <div class="section text-center mt-2">
                         <h4>Jadilah bagian dari GaramApp<br>dengan menjadi calon pembeli cerdas.</h4>
@@ -200,7 +200,7 @@ buyer
 
             <!-- Seller Register -->
             <div class="tab-pane fade" id="lined" role="tabpanel">
-seller
+<!-- seller -->
                 <div class="section full mt-1">
                     <div class="section text-center mt-2">
                         <h4>Jadilah bagian dari GaramApp<br>dengan menjadi penyedia stok garam terpercaya..</h4>
